209078be5bb2fe0724f979d11ea8a8d37d360631
Ravonzz774 <ravonzz@wwp.red>
2023-12-13 21:42:51 +0500

    first commit

---
+++ components/layout/Sidebar.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <aside class="px-5 py-8 bg-sidebar h-full relative w-full">
+    <NuxtLink to="/" class="mb-10 block mx-auto">
+      <NuxtImg src="/logo.png" alt="" width="140px" class="mx-auto" />
+    </NuxtLink>
+
+    <LayoutMenu />
+  </aside>
+</template>
+
+<style scoped></style>
---
+++ nuxt.config.ts
@@ -0,0 +1,30 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: false },
+  modules: [
+    "@nuxt/image",
+    "@nuxtjs/tailwindcss",
+    "shadcn-nuxt",
+    "nuxt-icon",
+    "@pinia/nuxt",
+    [
+      "@nuxtjs/google-fonts",
+      {
+        families: {
+          Ubuntu: {
+            wght: [300, 400, 700],
+            ital: [300],
+          },
+        },
+      },
+    ],
+  ],
+  shadcn: {
+    prefix: "Ui",
+
+    componentDir: "./components/ui",
+  },
+  pinia: {
+    storesDirs: ["./store/**"],
+  },
+});
